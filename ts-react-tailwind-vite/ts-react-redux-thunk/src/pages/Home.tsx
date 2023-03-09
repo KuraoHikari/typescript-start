454964f81a3354ce901064cd537a140324379475
KuraoHikari <dewaindra705@gmail.com>
2023-03-09 15:04:43 +0800

    hooks setup

--- ts-react-tailwind-vite/ts-react-redux-thunk/src/pages/Home.tsx
+++ ts-react-tailwind-vite/ts-react-redux-thunk/src/pages/Home.tsx
@@ -1,7 +1,19 @@
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
+
 type Props = {};
 
 const Home = (props: Props) => {
-  return <div>Home</div>;
+  return (
+    <div className="max-h-screen overflow-hidden">
+      <div style={{ height: '6vh' }}>
+        <Navbar />
+      </div>
+      <div className="flex" style={{ height: '94vh' }}>
+        <Sidebar />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
