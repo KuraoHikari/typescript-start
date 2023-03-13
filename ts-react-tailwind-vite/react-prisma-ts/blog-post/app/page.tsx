3ee0b4cdd8b86dbf82fd84f904bf78c7d941c2b4
KuraoHikari <dewaindra705@gmail.com>
2023-03-13 11:40:24 +0800

    create post and auth session done

--- ts-react-tailwind-vite/react-prisma-ts/blog-post/app/page.tsx
+++ ts-react-tailwind-vite/react-prisma-ts/blog-post/app/page.tsx
@@ -1,7 +1,12 @@
+'use client';
+
+import AddPost from './components/AddPost';
+
 export default function Home() {
   return (
     <main>
       <h1 className="text-sm">Hello World</h1>
+      <AddPost />
     </main>
   );
 }
