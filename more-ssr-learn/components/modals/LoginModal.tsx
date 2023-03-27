55824a2180ed5966e29d6bc4547de24f56a76a8d
KuraoHikari <dewaindra705@gmail.com>
2023-03-27 16:21:30 +0800

    update

--- more-ssr-learn/components/modals/LoginModal.tsx
+++ more-ssr-learn/components/modals/LoginModal.tsx
@@ -1,20 +1,78 @@
 import useLoginModal from '@/hooks/useLoginModal';
 import { useCallback, useState } from 'react';
+import Input from '../Input';
+import Modal from '../Modal';
+import useRegisterModal from '@/hooks/useRegisterModal';
 
 const LoginModal = () => {
   const loginModal = useLoginModal();
+  const registerModal = useRegisterModal();
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
   const onSubmit = useCallback(async () => {
-    setIsLoading(true);
+    try {
+      setIsLoading(true);
 
-    loginModal.onClose();
+      loginModal.onClose();
+    } catch (error) {
+      console.log('🚀 ~ file: LoginModal.tsx:18 ~ onSubmit ~ error:', error);
+    } finally {
+      setIsLoading(false);
+    }
   }, [email, password, loginModal]);
 
-  return <div></div>;
+  const onToggle = useCallback(() => {
+    loginModal.onClose();
+    registerModal.onOpen();
+  }, [loginModal, registerModal]);
+
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Input
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+        disabled={isLoading}
+      />
+      <Input
+        placeholder="Password"
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+        disabled={isLoading}
+      />
+    </div>
+  );
+
+  const footerContent = (
+    <div className="text-neutral-400 text-center mt-4">
+      <p>
+        First time using Twitter?
+        <span
+          onClick={onToggle}
+          className="text-white cursor-pointer hover:underline"
+        >
+          Create an account
+        </span>
+      </p>
+    </div>
+  );
+
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={loginModal.isOpen}
+      title="Login"
+      actionLabel="Sign in"
+      onClose={loginModal.onClose}
+      onSubmit={onSubmit}
+      body={bodyContent}
+      footer={footerContent}
+    />
+  );
 };
 
 export default LoginModal;
