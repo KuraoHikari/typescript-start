e6b1a9303cc5b8832411008a4f8c20f587311927
KuraoHikari <dewaindra705@gmail.com>
2023-03-30 22:33:53 +0800

    update edit modal

---
+++ more-ssr-learn/components/modals/EditModal.tsx
@@ -0,0 +1,35 @@
+import useCurrentUser from '@/hooks/useCurrentUser';
+import useEditModal from '@/hooks/useEditModal';
+import useUser from '@/hooks/useUser';
+import { useCallback, useEffect, useState } from 'react';
+
+const EditModal = () => {
+  const { data: currentUser } = useCurrentUser();
+  const { mutate: mutateFetchedUser } = useUser(currentUser?.id);
+  const editModal = useEditModal();
+
+  const [profileImage, setProfileImage] = useState('');
+  const [coverImage, setCoverImage] = useState('');
+  const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
+  const [bio, setBio] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setProfileImage(currentUser?.profileImage);
+    setCoverImage(currentUser?.coverImage);
+    setName(currentUser?.name);
+    setUsername(currentUser?.username);
+    setBio(currentUser?.bio);
+  }, [
+    currentUser?.name,
+    currentUser?.username,
+    currentUser?.bio,
+    currentUser?.profileImage,
+    currentUser?.coverImage,
+  ]);
+
+  const onSubmit = useCallback(async () => {}, []);
+  return <div></div>;
+};
+export default EditModal;
