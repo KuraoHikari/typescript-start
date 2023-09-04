eade465994b5378449325e2c42569b87589d0da5
KuraoHikari <dewaindra705@gmail.com>
2023-09-04 19:24:59 +0800

    server header finish

--- dc-c/app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ dc-c/app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -4,7 +4,6 @@ import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
 import { currentProfile } from "@/lib/current-profile";
 import ServerSidebar from "@/components/server/server-sidebar";
-// import { ServerSidebar } from "@/components/server/server-sidebar";
 
 const ServerIdLayout = async ({
  children,
