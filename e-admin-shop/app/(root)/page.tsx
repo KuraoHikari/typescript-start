bdcf32257d82a2a5580c8cf8bea8c78225b162aa
KuraoHikari <dewaindra705@gmail.com>
2023-09-27 14:11:34 +0800

    update

---
+++ e-admin-shop/app/(root)/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
+
+export default function Root() {
+ const onOpen = useStoreModal((state) => state.onOpen);
+ const isOpen = useStoreModal((state) => state.isOpen);
+
+ useEffect(() => {
+  if (!isOpen) {
+   onOpen();
+  }
+ }, [isOpen, onOpen]);
+ return <div></div>;
+}
