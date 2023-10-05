de0049c8e604c1480cdb8523bd755b36ccb66cd2
KuraoHikari <dewaindra705@gmail.com>
2023-10-05 16:36:03 +0800

    update

--- e-admin-shop/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ e-admin-shop/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,6 +1,13 @@
 "use client";
 
 import * as z from "zod";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { Store } from "@prisma/client";
+import { Trash } from "lucide-react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -12,13 +19,12 @@ import {
  FormMessage,
 } from "@/components/ui/form";
 import { Heading } from "@/components/ui/heading";
-import Separator from "@/components/ui/separator";
-import { Store } from "@prisma/client";
-import { Trash } from "lucide-react";
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { useParams, useRouter } from "next/navigation";
+import { AlertModal } from "@/components/modals/alert-modal";
+import { ApiAlert } from "@/components/ui/api-alert";
+import { useOrigin } from "@/hooks/use-origin";
 
 interface SettingsFormProps {
  initialData: Store;
@@ -32,14 +38,59 @@ type SettingsFormValues = z.infer<typeof formSchema>;
 export const SettingsForm: React.FC<SettingsFormProps> = ({
  initialData,
 }) => {
+ const params = useParams();
+ const router = useRouter();
+ const origin = useOrigin();
+
+ const [open, setOpen] = useState(false);
  const [loading, setLoading] = useState(false);
 
  const form = useForm<SettingsFormValues>({
   resolver: zodResolver(formSchema),
   defaultValues: initialData,
  });
+
+ const onSubmit = async (data: SettingsFormValues) => {
+  console.log(
+   "🚀 ~ file: settings-form.tsx:44 ~ onSubmit ~ data:",
+   data
+  );
+  try {
+   setLoading(true);
+   await axios.patch(`/api/stores/${params.storeId}`, data);
+   router.refresh();
+   toast.success("Store updated.");
+  } catch (error) {
+   toast.error("Something went wrong.");
+  } finally {
+   setLoading(false);
+  }
+ };
+
+ const onDelete = async () => {
+  try {
+   setLoading(true);
+   await axios.delete(`/api/stores/${params.storeId}`);
+   router.refresh();
+   router.push("/");
+   toast.success("Store deleted.");
+  } catch (error) {
+   toast.error(
+    "Make sure you removed all products and categories first."
+   );
+  } finally {
+   setLoading(false);
+   setOpen(false);
+  }
+ };
  return (
   <>
+   <AlertModal
+    isOpen={open}
+    onClose={() => setOpen(false)}
+    onConfirm={onDelete}
+    loading={loading}
+   />
    <div className="flex items-center justify-between">
     <Heading
      title="Settings"
@@ -49,13 +100,17 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
      disabled={loading}
      variant="destructive"
      size={"sm"}
+     onClick={() => setOpen(true)}
     >
      <Trash className="h-4 w-4" />
     </Button>
    </div>
    <Separator />
    <Form {...form}>
-    <form className="space-y-8 w-full">
+    <form
+     onSubmit={form.handleSubmit(onSubmit)}
+     className="space-y-8 w-full"
+    >
      <div className="grid grid-cols-3 gap-8">
       <FormField
        control={form.control}
@@ -75,8 +130,21 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
        )}
       />
      </div>
+     <Button
+      disabled={loading}
+      className="ml-auto"
+      type="submit"
+     >
+      Save changes
+     </Button>
     </form>
    </Form>
+   <Separator />
+   <ApiAlert
+    title="NEXT_PUBLIC_API_URL"
+    variant="public"
+    description={`${origin}/api/${params.storeId}`}
+   />
   </>
  );
 };
